b01db307df058e4baa0dbd8ce4d5f9da2b4d2684
AlphaHon <79430911+AlphaHon@users.noreply.github.com>
2021-02-24 19:20:14 -0600

    d

--- js/formulario.js
+++ js/formulario.js
@@ -88,7 +88,7 @@ formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
 
 	const terminos = document.getElementById('terminos');
-	if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked ){
+	if(campos.usuario && campos.nompasbre && campos.sword && campos.correo && campos.telefono && terminos.checked ){
 		formulario.reset();
 
 		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
